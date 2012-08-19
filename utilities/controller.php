64bca992c5fc008b75f00d2a9d9517a3b11ed2fd
Eric Stern <firehed@gmail.com>
2012-08-19 23:14:20 +0000

    Change controller interface to abstract class
    * Forbid use of magic methods __construct and __destruct
    * Declare index_get as abstract (default route)
    * Create empty setUp and tearDown functions

--- utilities/controller.php
+++ utilities/controller.php
@@ -1,15 +1,22 @@
 <?php
 
-interface Controller {
+abstract class Controller {
 
 	// This is the default method if one is not specified by the request
-	function index_get();
+	abstract public function index_get();
 
 	// This will be called before the method. Use this in favor of __construct
-	function setUp();
+	public function setUp() {
+		// Override me
+	}
 
 	// This will be called after the method. Use this in favor of __destruct (more predictable due to PHP's GC)
-	function tearDown();
+	public function tearDown() {
+		// Override me
+	}
+
+	// Force use of setUp and tearDown
+	final public function __construct() {}
+	final public function __destruct() {}
 
 }
-
